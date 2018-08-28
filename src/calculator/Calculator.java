ea9dc143dedd36aac6cc8c937e1755c9c6c64360
Dimitry Fedotov <dima@fedoto.ws>
2018-08-28 18:51:02 +0300

    can calculate long expression + and - only

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -8,36 +8,22 @@ package calculator;
 class Calculator {
 
     String calculate(String[] expression) {
-        System.out.println(expression[0]);
-        System.out.println(expression[1]);
-        System.out.println(expression[2]);
-        int a = Integer.parseInt(expression[0]);
-        int b = Integer.parseInt(expression[2]);
-        int result;
-        switch (expression[1]) {
-            case "+":
-                result = a + b;
-                break;
-            case "-":
-                result = a - b;
-                break;
-            default:
-                return "ERROR";
+        double result = Double.parseDouble(expression[0]);
+        for (int i = 1; i < expression.length; i += 2) {
+            String op = expression[i];
+            double b = Double.parseDouble(expression[i + 1]);
+            switch (op) {
+                case "+":
+                    result += b;
+                    break;
+                case "-":
+                    result -= b;
+                    break;
+                default:
+                    return "ERROR";
+            }
         }
-        if (expression.length < 4) {
-            return String.valueOf(result);
-        }
-        int c = Integer.parseInt(expression[4]);
-        switch (expression[3]) {
-            case "+":
-                result = result + c;
-                break;
-            case "-":
-                result = result - c;
-                break;
-            default:
-                return "ERROR";
-        }
+
         return String.valueOf(result);
     }
 
